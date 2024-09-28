969dfae64e1b292a857a0ab9b24fe5206fe883eb
Divyansh <165415580+divyansh-1503@users.noreply.github.com>
2024-09-28 14:29:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.set_page_config(page_title="My Webpage",page_icon=":☸⚛☸:",layout="wide")
+
+st.subheader("Hello, I am Divyansh :🕵:")
+st.title("A Btech CSE Student from India")
+st.write("I am deeply passionate about learning new technologies.")
+st.write("[Learn More:](https://linktr.ee/divyansh1503)")
